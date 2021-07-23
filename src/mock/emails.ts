f23fd4ced428bb4386b34080109492503a3ca828
m7he4rt <mateuspm2015@hotmail.com>
2021-07-23 15:51:00 -0300

    feat: I created mocked emails

---
+++ src/mock/emails.ts
@@ -0,0 +1,12 @@
+import { ItemProps } from "@/common/Item";
+
+export const emails: ItemProps[] = [
+  {
+    email: "contato@rarolabs.com.br",
+    id: "1",
+  },
+  {
+    email: "nao-responda@rarolabs.com.br",
+    id: "2"
+  }
+]
